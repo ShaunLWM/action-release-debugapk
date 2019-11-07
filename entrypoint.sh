430580aef8f08671721eb6fd8b665e998bb3b224
tank777 <bhavesh.tank777@gmail.com>
2019-11-07 15:07:33 +0530

    Fix apk file name
    It should be the any file name with .apk

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 hub checkout master
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*.apk -m "${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
