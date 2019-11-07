d53618eec1b9032be4d6b0148356938226d5881a
ShaunLWM <shaunlimweimin@gmail.com>
2019-11-07 22:56:14 +0800

    Test grepping of version name into title

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 hub checkout master
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
+VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE} - ${VERSION_NAME}" $(date +%Y%m%d%H%M%S) 
